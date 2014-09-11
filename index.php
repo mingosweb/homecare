144ec28feec0411132b7bde2d851915fd7909c3e
mingosweb <mingox123@yahoo.es>
2014-09-11 00:26:33 -0500

    Agregar parrafo
    Este es el primer cambio que le hacemos al archivo index.php. Agregamos parrafo y en el proximo cambio agregaremos otro para conocer los cambios que se van ahciendo.

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
       <TITLE>www.google.com</TITTLE>
   </HEAD>
   <BODY>
-    <H3>Hola Mundp!</H3>
+    <H3>Hola Mundo!</H3>
+    <P>Con este parrafo quiero introducir un cambio al archivo</P>
   </BODY>
 </HTML>
